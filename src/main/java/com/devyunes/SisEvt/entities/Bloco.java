93dfb487ef5fdd173cfceba9c263333d17fd3fa9
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-06-25 10:27:53 -0400

    Create Atividade, Bloco, Categoria, Participante.

---
+++ src/main/java/com/devyunes/SisEvt/entities/Bloco.java
@@ -0,0 +1,63 @@
+package com.devyunes.SisEvt.entities;
+
+import java.time.Instant;
+import java.util.Objects;
+
+public class Bloco {
+	private Integer id;
+	private Instant inicio;
+	private Instant fim;
+	
+	public Bloco() {
+	}
+
+	public Bloco(Integer id, Instant inicio, Instant fim) {
+		this.id = id;
+		this.inicio = inicio;
+		this.fim = fim;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Instant inicio) {
+		this.inicio = inicio;
+	}
+
+	public Instant getFim() {
+		return fim;
+	}
+
+	public void setFim(Instant fim) {
+		this.fim = fim;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bloco other = (Bloco) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+}
