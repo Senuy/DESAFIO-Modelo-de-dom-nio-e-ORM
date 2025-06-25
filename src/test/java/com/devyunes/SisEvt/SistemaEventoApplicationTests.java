cd24c9ecf50203522112b69a78bb8ec7719a5d0e
Yunes da Silva Santos <yunessilva_santos@hotmail.com>
2025-06-25 09:57:45 -0400

    Create Project

---
+++ src/test/java/com/devyunes/SisEvt/SistemaEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devyunes.SisEvt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
